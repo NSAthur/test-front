601a3873311ec8b2157279dedc8a83676cde8fca
NSAthur <nemorsen@gmail.com>
2023-03-30 21:19:23 +0800

    学习layui

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const Login = () => import('@/views/Login.vue')
+const Reg = () => import('@/views/Reg.vue')
+const Forget = () => import('@/views/Forget.vue')
+
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/reg',
+      name: 'reg',
+      component: Reg,
+      beforeEnter: (to, from, next) => {
+        if (from.name === 'login') {
+          next()
+        } else {
+          next('/login')
+        }
+      }
+    },
+    {
+      path: '/forget',
+      name: 'forget',
+      component: Forget
+    }
+  ]
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,81 @@
-
 <template>
   <div>
-    <imooc-panel></imooc-panel>
-    <router-view></router-view>
+    <div class="panel">
+      <div class="layui-container">
+        <ul>
+          <li class="layui-hide-xs"><a href="">首页</a></li>
+          <li><a href="">提问</a></li>
+          <li><a href="">分享</a></li>
+          <li><a href="">讨论</a></li>
+          <li><a href="">建议</a></li>
+          <li><a href="">公告</a></li>
+          <li><a href="">动态</a></li>
+          <li class="layui-hide-xs layui-hide-ms"><span class="line"></span></li>
+          <li class="layui-hide-xs layui-hide-ms"><a href="">我发表的贴</a></li>
+          <li class="layui-hide-xs layui-hide-ms"><a href="">我的收藏</a></li>
+        </ul>
+        <div class="right layui-hide-xs layui-hide-ms">
+            <span class="layui-icon layui-icon-search"></span>
+            <a href="" class="layui-btn">发表新帖</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Panel from '@/components/Panel'
-
 export default {
-  name: 'home',
-  components: {
-    'imooc-panel': Panel
-  }
+  name: 'home'
 }
 </script>
 
 <style lang="scss" scoped>
+@media (max-width:768px) {
+  .panel{
+    height: auto !important;
+  }
+  ul{
+    li{
+      width:33.3%;
+    }
+  }
+}
+.panel {
+    position: relative;
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 15px;
+  border-radius: 2px;
+  background-color: #fff;
+  box-shadow: 0 1px 2px 0 rgba($color: #000000, $alpha: 0.05);
+}
+ul {
+  li {
+    display: inline-block;
+    a {
+      padding: 0 20px;
+    }
+  }
+  .line {
+    display: inline-block;
+    height: 10px;
+    width: 1px;
+    vertical-align: middle;
+    margin: 0 20px;
+    background: #e2e2e2;
+  }
+}
+.right{
+    position: absolute;
+    right: 0;
+    top: 0;
+    .layui-icon-search{
+        margin-right: 10px;
+        font-size: 20px;
+        &:hover{
+          color: #5fb878;
+          cursor: pointer;
+        }
+    }
+}
 </style>
